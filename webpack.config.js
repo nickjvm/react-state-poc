8deb08b6f0ba48aa71fad15d67584ae4121cd943
Nick V <nickjvm@gmail.com>
2023-08-28 09:12:00 -0600

    initial commit; bootstrap react

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  return{
+    entry: './src/index.jsx',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      publicPath: '/',
+      filename: 'bundle.js',
+    },
+    resolve: {
+      alias: {
+        components: path.resolve(__dirname, 'src'),
+      },
+      extensions: ['.js', '.jsx'],
+    },
+    devServer: {
+      liveReload: true,
+      // contentBase: "./build",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader', 'eslint-loader'],
+        },
+        {
+          test: /\.less$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'less-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template:  path.resolve('./index.html'),
+      }),
+    ],
+    devtool : isProduction ?'source-map' : 'inline-source-map',
+  };
+
+};
