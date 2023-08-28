30cc220a745f175f88ea4406c3d82768a2313c39
Nick V <nickjvm@gmail.com>
2023-08-28 13:59:34 -0600

    remove AuthContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,45 @@
-import React from 'react';
-import AuthContextProvider from '@/lib/AuthContext';
+import React, { useState } from 'react';
 
-import Welcome from '@/components/pages/Welcome';
+import UserProfile from '@/components/molecules/UserProfile';
+import SignUp from '@/components/molecules/SignUp';
 
 import './App.scss';
 
-function App() {
-  return (
-    <AuthContextProvider>
-      <Welcome />
-    </AuthContextProvider>
-  );
-}
+export default function App() {
+  const [user, setUser] = useState(null);
 
-export default App;
+  const createUser = async (user) => {
+    return fetch('https://dummyjson.com/users/add', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: user.username.trim(),
+        // for this demo, we're not going to send a password over the wire to a mock API
+        // password: user.password,
+      }),
+    })
+      .then(r => r.json())
+      .then(r => {
+        setUser(r);
+        localStorage.setItem('user', JSON.stringify(r));
+        return {
+          success: true,
+          user: r,
+        };
+      }).catch(error => {
+        throw {
+          success: false,
+          error: {
+            code: 'FAILED',
+            details: error.message,
+          },
+        };
+      });
+  };
+
+  if (!user) {
+    return <SignUp createUser={createUser} />;
+  } else {
+    return <UserProfile user={user} />;
+  }
+}
